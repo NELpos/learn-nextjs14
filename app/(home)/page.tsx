7868fd0604f792aa321e31b32810ed1e3d049905
Nelpos <nelpos3959@gmail.com>
2024-07-23 12:54:37 +0900

    append css

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
     title : "Home"
@@ -13,15 +14,18 @@ async function getMovies() {
     return json
 }
 
-export default async function Page() {    
-    const movies = await getMovies();
-    return (
-        <div>
-          {movies.map((movie) => (
-            <li key={movie.id}>
-              <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-            </li>
-          ))}
-        </div>
-      );
+export default async function HomePage() {
+  const movies = await getMovies();
+  return (
+    <div className={styles.container}>
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
+      ))}
+    </div>
+  );
 }
